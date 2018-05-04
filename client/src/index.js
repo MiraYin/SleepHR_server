8d62183cb33804400ddfb740558be290a9ea5a3d
yanxin <yanxin@cs.dartmouth.edu>
2018-05-03 20:13:01 -0400

    init

---
+++ client/src/index.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import axios from 'axios';
+import ReactDOM from 'react-dom';
+
+class StayUp extends React.Component{
+    constructor(props){
+        super(props);
+        this.handlerOnChange = this.handlerOnChange.bind(this);
+    }
+    
+    handlerOnChange(event){
+        this.props.handlerOnReasonChange(event.target.value);
+    }
+    
+    render(){
+        return (
+          <div>
+        <label>
+            What was your reason in general to stay up?
+            <select onChange={this.handlerOnChange}>
+                <option value="work">work</option>
+                <option value="read">read</option>
+                <option value="watch videos">watch videos</option>
+                <option value="play games">play games</option>
+                <option value="chat with friends">chat with friends</option>
+                <option value="others">others</option>
+            </select>
+        </label>
+            </div>
+        );
+    }
+  }
+  class Survey extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            sleepQuality: 0,
+            stayUpFlag: false,
+            stayUpReason: null
+        }
+        this.handlerFlagOnChange=this.handlerFlagOnChange.bind(this);
+      this.handlerQualityOnChange=this.handlerQualityOnChange.bind(this);
+        this.handlerOnSubmit=this.handlerOnSubmit.bind(this);
+        this.handlerOnReasonChange=this.handlerOnReasonChange.bind(this);
+    }
+    
+    handlerOnSubmit(event){
+        let output = "sleepQuality: "+this.state.sleepQuality+(this.state.stayUpFlag ? "\nStay up, for "+this.state.stayUpReason:"\nOn bedtime schedule");
+        console.log(output);
+        // event.preventDefault();
+
+        axios.post(`http://localhost:5000/api/survey`, {
+            sleepQuality: this.state.sleepQuality,
+            StayUp: this.state.stayUpFlag,
+        }).then(res => {
+          console.log(res);
+          console.log(res.data);
+        })
+        event.preventDefault();
+    }
+    
+    handlerFlagOnChange(event){
+        this.setState({
+            stayUpFlag: event.target.checked
+        });
+    }
+    
+    handlerQualityOnChange(event){
+        this.setState({
+            sleepQuality: event.target.value
+        });
+    }
+    handlerOnReasonChange(reason){
+        this.setState({
+            stayUpReason: reason
+        });
+    }
+    render(){
+        return(
+        <form onSubmit={this.handlerOnSubmit}>
+            <h1>Survey</h1>
+            <label>How was your sleep quality last night? Scale from 0-4
+                <select onChange={this.handlerQualityOnChange}>
+                    <option value='0'>0</option>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                    <option value='4'>4</option>
+                </select>
+            </label>
+            <br />
+            <label>Did you go to sleep as scheduled?
+                <input type="checkbox" onChange={this.handlerFlagOnChange}/>
+            </label>
+            <br />
+            {this.state.stayUpFlag ? <StayUp handlerOnReasonChange={this.handlerOnReasonChange}/> : null}
+            <input type="submit" value="Submit"/>
+        </form>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <Survey />,
+    document.getElementById('root')
+  );
---
+++ models/survey.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+/// SCHEMA SETUP
+const surveySchema = new Schema({
+  sleepQuality:{
+    type: mongoose.Schema.Types.Number,
+    required: true,
+  },
+  stayUp:{
+    type: mongoose.Schema.Types.Boolean,
+    required: true,
+  },
+  stayUpReason:{
+    type: mongoose.Schema.Types.String,
+    required: false,
+  }
+})
+
+/// surveySchema is a pattern; Survey is a compiled model with methods
+/// Survey.create; Survey.update
+const Survey = mongoose.model('Survey',surveySchema);
+
+module.exports = Survey;
+
+
